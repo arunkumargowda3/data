2124757de17d3ba03f267d495898f0048b1c7eba
arunkumargowda3 <arunkumargowda3@gmail.com>
2023-11-06 22:57:29 +0530

    demo

---
+++ security.tf
@@ -0,0 +1,27 @@
+#create security group for an Instance
+resource "aws_security_group" "SG" {
+  description = "Security group for ${var.env} ${var.instance_name}"
+  name        = "${var.instance_name}-${var.env}-SG"
+  vpc_id      = var.vpc
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
